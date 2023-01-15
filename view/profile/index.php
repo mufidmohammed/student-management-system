8c7c8ec2fd236b3a79af108916258d741f1c88a0
Mufid Fusheini <mufidmohammed200@gmail.com>
2023-01-15 02:51:40 +0000

    redesigned the profile page

--- view/profile/index.php
+++ view/profile/index.php
@@ -4,7 +4,19 @@ include_once('../partials/header.php');
 include_once('../partials/sidebar.php');
 
 ?>
+<style>
+#content main .form-group {
+    display: flex;
+    justify-content: space-between;
+    align-items: top;
+}
 
+.form-group .form-info {
+    margin-top: 50px;
+    margin-left: 25px;
+    color: var(--dark);
+}
+</style>
 <section id="content">
     <?php include_once('../partials/navbar.php') ?>
 
@@ -29,60 +41,84 @@ include_once('../partials/sidebar.php');
             </a>
         </div>
 
-
-        <div class="table-data">
-            <div class="form-data">
-                <div class="form-head">Update Profile</div>
-                <form action="" method="post">
-                    <div class="input-field">
-                        <label for="name">First name</label>
-                        <input type="text" name="first_name" value="" class="form-control">
-                    </div>
-                    <div class="input-field">
-                        <label for="name">Last name</label>
-                        <input type="text" name="last_name" value="" class="form-control">
-                    </div>
-                    <div class="input-field">
-                        <label for="email">Email</label>
-                        <input type="email" name="email" value="" class="form-control">
-                    </div>
-                    <div class="input-field">
-                        <label for="password">Password</label>
-                        <input type="text" name="password" value="" class="form-control">
-                    </div>
-                    <div class="input-field">
-                        <label for="phoneNumber">Phone number</label>
-                        <input type="number" name="phone_number" value="" class="form-control">
-                    </div>
-                    <div class="input-field">
-                        <label for="dateOfBirth">Date of birth</label>
-                        <input type="date" name="date_of_birth" value="" class="form-control">
-                    </div>
-                    <div class="input-field">
-                        <label for="gender">Gender:</label>
-                        <input type="radio" name="gender" value="male" class="form-radio"> Male
-                        <input type="radio" name="gender" value="female" class="form-radio"> Female
-                    </div>
-                    <div class="input-field">
-                        <label for="name">Address</label>
-                        <input type="text" name="address" value="" class="form-control">
-                    </div>
-                    <div class="input-field">
-                        <label for="city">City</label>
-                        <input type="text" name="city" value="" class="form-control">
-                    </div>
-                    <div class="input-field">
-                        <label for="region">Region</label>
-                        <input type="text" name="region" value="" class="form-control">
-                    </div>
-                    <div class="form-submit">
-                        <button class="submit-button" role="submit">Update</button>
-                    </div>
-                </form>
+        <!-- Personal Information -->
+        <div class="form-group">
+            <div class="form-info">Personal Information</div>
+            <div class="table-data">
+                <div class="form-data">
+                    <form action="" method="post">
+                        <div class="input-field">
+                            <label for="name">First name</label>
+                            <input type="text" name="first_name" value="" class="form-control" required="">
+                        </div>
+                        <div class="input-field">
+                            <label for="name">Last name</label>
+                            <input type="text" name="last_name" value="" class="form-control" required="">
+                        </div>
+                        <div class="input-field">
+                            <label for="dateOfBirth">Date of birth</label>
+                            <input type="date" name="date_of_birth" value="" class="form-control" required="">
+                        </div>
+                        <div class="input-field">
+                            <label for="gender">Gender:</label>
+                            <input type="radio" name="gender" value="male" class="form-radio"> Male
+                            <input type="radio" name="gender" value="female" class="form-radio"> Female
+                        </div>
+                        <div class="form-submit">
+                            <button class="submit-button" role="submit">Save</button>
+                        </div>
+                    </form>
+                </div>
             </div>
-
         </div>
 
+        <!-- contact information -->
+        <div class="form-group">
+            <div class="form-info">Contact Information</div>
+            <div class="table-data">
+                <div class="form-data">
+                    <form action="" method="post">
+                        <div class="input-field">
+                            <label for="email">Email</label>
+                            <input type="email" name="email" value="" class="form-control" required="">
+                        </div>
+                        <div class="input-field">
+                            <label for="phoneNumber">Phone number</label>
+                            <input type="number" name="phone_number" value="" class="form-control" required="">
+                        </div>
+                        <div class="form-submit">
+                            <button class="submit-button" role="submit">Save</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+
+        <!-- location information -->
+        <div class="form-group">
+            <div class="form-info">Contact Information</div>
+            <div class="table-data">
+                <div class="form-data">
+                    <form action="" method="post">
+                        <div class="input-field">
+                            <label for="address">Address</label>
+                            <input type="address" name="address" value="" class="form-control" required="">
+                        </div>
+                        <div class="input-field">
+                            <label for="city">City</label>
+                            <input type="text" name="city" value="" class="form-control" required="">
+                        </div>
+                        <div class="input-field">
+                            <label for="region">Region</label>
+                            <input type="text" name="region" value="" class="form-control" required="">
+                        </div>
+                        <div class="form-submit">
+                            <button class="submit-button" role="submit">Save</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
     </main>
     <!-- MAIN -->
 
